b19432faa4314d6fe26579fade1b210217014a97
LucasFR-TH <lucas.f.roldan@gmail.com>
2025-06-03 18:00:18 -0300

    construccion de todo el interfaz de el ejercicio 1

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -5,25 +5,91 @@ Tarea tareas = new Tarea();
 
 string descripciones[] = {"Sacar basura"; "Realizar mesa"; "Pintar pieza"; "Contar dinero"; "Activar maquina"};
 
-Console.WriteLine("--------------------------------------");
-Console.WriteLine("..........REGISTRO DE TAREAS..........");
-Console.WriteLine("--------------------------------------");
-
 int numeroTareas = random.Next(11);
 List<tareas> tareasPendientes = new List<tareas>();
+List<tareas> tareasRealizadas = new List<tareas>();
 
 for (int i=0; i<numeroTareas; i++) {
     tareasPendientes.add(new Tarea(i, descripciones[random.next(5)], random.next(100)));
 }
 
-// muestro la lista de tareas
-foreach (Tarea tarea in tareasPendientes) {
-    Console.WriteLine($"ID de tarea: {tareas.TareaID}");
-    Console.WriteLine($"Descripcion: {tareas.Descripcion}");
-    Console.WriteLine($"Duracion: {tareas.Duracion}");
-    Console.WriteLine();
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("..........REGISTRO DE TAREAS..........");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine();
+int opern = 1;
+while (opern != 0) {
+    Console.WriteLine("Que desea realizar?");
+    Console.WriteLine("1 - Mostrar tareas pendientes.");
+    Console.WriteLine("2 - Mostrar tareas realizadas.");
+    Console.WriteLine("3 - Busque una tarea por descripcion.");
+    Console.WriteLine("4 - Marcar una tarea como realizada.");
+
+    string oper = Console.ReadLine();
+
+    if (int.TryParse(oper, out opern)) {
+        if (opern != 0) {
+            switch (opern)
+                {
+                    case 1:
+                        Console.WriteLine("..........TAREAS PENDIENTES...........");
+                        Console.WriteLine();
+                        // muestro la lista de tareas
+                        foreach (Tarea tarea in tareasPendientes) {
+                            Console.WriteLine($"ID de tarea: {tareas.TareaID}");
+                            Console.WriteLine($"Descripcion: {tareas.Descripcion}");
+                            Console.WriteLine($"Duracion: {tareas.Duracion}");
+                            Console.WriteLine();
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("..........TAREAS REALIZADA...........");
+                        Console.WriteLine();
+                        // muestro la lista de tareas
+                        foreach (Tarea tarea in tareasRealizadas) {
+                            Console.WriteLine($"ID de tarea: {tareas.TareaID}");
+                            Console.WriteLine($"Descripcion: {tareas.Descripcion}");
+                            Console.WriteLine($"Duracion: {tareas.Duracion}");
+                            Console.WriteLine();
+                        }
+                    case 3:
+                        Console.WriteLine("Ingrese la tarea que desea encontrar:");
+                        string tareaBuscada = Console.ReadLine();
+                        // muestro la lista de tareas coincidnetes
+                        foreach (Tarea tarea in tareasPendientes) {
+                            if (tareas.Descripcion = tareaBuscada) {
+                                Console.WriteLine($"ID de tarea: {tareas.TareaID}");
+                                Console.WriteLine($"Descripcion: {tareas.Descripcion}");
+                                Console.WriteLine($"Duracion: {tareas.Duracion}");
+                                Console.WriteLine(); 
+                            }
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("Indique el ID de la tarea a marcar como realizada:");
+                        string tareaCambiar = Console.ReadLine();
+                        int NtareaCambiar;
+                        if (int.TryParse(tareaCambiar, out NtareaCambiar)) {
+                            foreach (Tarea tarea in tareasPendientes) {
+                                if (tareas.TareaID = NtareaCambiar) {
+                                    tareasRealizadas.add(new Tarea(tareas.TareaID, tareas.Descripcion, tareas.Duracion);
+                                    tarea = remove tarea;
+                                }
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+        }
 }
 
-List<tareas> tareasRealizadas = new List<tareas>();
+
+
+
+
+
+
+
 
 
